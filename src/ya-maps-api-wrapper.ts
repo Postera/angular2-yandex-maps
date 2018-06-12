5e5b313632c8c58a6bfc70ce62fb52bd6721cb2b
gritsev <gritsev@gmail.com>
2018-06-13 01:24:47 +0300

    Update ya-maps-api-wrapper.ts

--- src/ya-maps-api-wrapper.ts
+++ src/ya-maps-api-wrapper.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { Observer } from 'rxjs/Observer';
 
 import * as mapTypes from './ya-maps-types';
